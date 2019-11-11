89529ab38b2f75f677bfc585d67312bbceb73e7c
kennCK <kennettecanales@gmail.com>
2019-11-11 12:14:01 +0800

    fixed withdrawal

--- app/Http/Controllers/WithdrawController.php
+++ app/Http/Controllers/WithdrawController.php
@@ -11,13 +11,12 @@ use App\NotificationSetting;
 
 class WithdrawController extends APIController
 {
-
-    public $ledgerClass = 'App\Http\Controllers\LedgerController';
-    public $notificationClass = 'App\Http\Controllers\NotificationSettingController';
-    function __construct(){
-      $this->model = new Withdraw();
-    }
-    public function create(Request $request){
+  public $ledgerClass = 'App\Http\Controllers\LedgerController';
+  public $notificationClass = 'App\Http\Controllers\NotificationSettingController';
+  function __construct(){
+    $this->model = new Withdraw();
+  }
+  public function create(Request $request){
       $response = array(
         'data'  => null,
         'otp'   => null,
@@ -33,7 +32,6 @@ class WithdrawController extends APIController
         $response['error'] = 'You have insufficient balance. Your balance is PHP '.$myBalance.' balance.';
       }else{
         if($data['otp'] == 0){
-          // request
           $code = app($this->notificationClass)->generateOTPFundTransfer($data['account_id']);
           $response['otp'] = true;
         }else if($data['otp'] == 1){
@@ -46,54 +44,26 @@ class WithdrawController extends APIController
           $Withdraw->payload_value = $data['payload_value'];
           $Withdraw->save();
           $response['data'] = $Withdraw->id;
-         // $response['error'] = null;
-         // $description = 'Invested to';
-          //  $payload = 'withdrawal';
-          //  $payloadValue = 'test';
-          // app($this->ledgerClass)->addToLedger($data['account_id'], $amount * (-1), $description, $payload, $payloadValue); 
-           // //     sent email
-           //      $details = array(
-           //        'title' => $description.' the amount of PHP'.number_format(($amount * (-1)), 2),
-           //        'transaction_id' => $code
-           //      );
+        }
+      }
+    return response()->json($response);
+  }
 
-           //      $subject = $amount.' has been invested in your account';
-      // app('App\Http\Controllers\EmailController')->investment($accountId, $details, $subject);  
+  public function retrieve(Request $request){
+    $data = $request->all();
+    $this->model = new Withdraw();
+    $this->retrieveDB($data);
+    $result = $this->response['data'];
+    return $this->response();
+  }
 
-      return response()->json($response);
+    public function generateCode(){
+    $code = substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 32);
+    $codeExist = Withdraw::where('code', '=', $code)->get();
+    if(sizeof($codeExist) > 0){
+      $this->generateCode();
+    }else{
+      return $code;
     }
   }
-}
-
-
-    //   public function update(Request $request){
-    //   $response = array(
-    //     'data'  => null,
-    //     'error' => null,
-    //     'timestamps' => Carbon::now()
-    //   );
-    //   $data = $request->all();
-
-    //   if($this->otpCode == $data['otpCode']){
-
-    //   }
-    // }
-
-    public function retrieve(Request $request){
-      $data = $request->all();
-      $this->model = new Withdraw();
-      $this->retrieveDB($data);
-      $result = $this->response['data'];
-      return $this->response();
-    }
-  
-      public function generateCode(){
-      $code = substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 32);
-      $codeExist = Withdraw::where('code', '=', $code)->get();
-      if(sizeof($codeExist) > 0){
-        $this->generateCode();
-      }else{
-        return $code;
-      }
-    }
 }
