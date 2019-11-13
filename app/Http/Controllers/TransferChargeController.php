edb63a4defca7c913b100f5f1c37409986ff0fc1
kennCK <kennettecanales@gmail.com>
2019-11-13 17:52:53 +0800

    update

--- app/Http/Controllers/TransferChargeController.php
+++ app/Http/Controllers/TransferChargeController.php
@@ -9,4 +9,20 @@ class TransferChargeController extends APIController
   function __construct(){
     $this->model = new TransferCharge();
   }
+
+
+  public function retrieve(Request $request){
+    $data = $request->all();
+    $this->model = new TransferCharge();
+    $this->retrieveDB($data);
+    $result = $this->response['data'];
+    if(sizeof($result) > 0){
+      $i = 0;
+      foreach ($result as $key) {
+        $this->response['data'][$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz('Asia/Manila')->format('F j, Y H:i A');
+        $i++;
+      }
+    }
+    return $this->response();
+  }
 }
