7e3c79bf20e6062bd0b4bdbc6e76d8273ed1758d
kennCK <kennettecanales@gmail.com>
2019-11-13 18:00:34 +0800

    udpate

--- app/Http/Controllers/TransferChargeController.php
+++ app/Http/Controllers/TransferChargeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\TransferCharge;
-use App\Carbon;
+use Carbon\Carbon;
 class TransferChargeController extends APIController
 {
   function __construct(){
