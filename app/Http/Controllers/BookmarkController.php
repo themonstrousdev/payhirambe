4c46c3201461fd25107b15c38cab0af79d6ae89f
kennCK <kennettecanales@gmail.com>
2019-12-02 17:28:20 +0800

    update

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Bookmark;
 class BookmarkController extends APIController
 {
+
+    public $requestClass = 'App\Http\Controllers\RequestMoneyController'; 
     function __construct(){
       $this->model = new Bookmark();
     }
@@ -28,5 +30,20 @@ class BookmarkController extends APIController
       $result = Bookmark::where('account_id', '=', $accountId)->where('request_id', '=', $requestId)->get();
 
       return (sizeof($result) > 0) ? $result[0]['id'] : null;
+    }
+
+    public function retrieve(Request $request){
+      $data = $request->all();
+      $this->model = new Bookmark();
+      $this->retrieveDB($data);
+      $result = $this->response['data'];
+      if(sizeof($result) > 0){
+        $i = 0;
+        foreach ($result as $key) {
+          $this->response['data'][$i]['request'] = app($this->requestClass)->retrieveById($result[$i]['request_id']);
+          $i++;
+        }
+      }
+      return $this->response();
     }
 }
