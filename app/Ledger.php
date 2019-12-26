77ca7540e41134c84c9ca2375aa01c4e6620f025
kennCK <kennettecanales@gmail.com>
2019-12-26 19:28:46 +0800

    update

--- app/Ledger.php
+++ app/Ledger.php
@@ -8,4 +8,12 @@ class Ledger extends APIModel
 {
   protected $table = 'ledgers';
   protected $fillable = ['account_id', 'code', 'amount', 'description', 'payload', 'payload_value'];
+
+  public function getAmountAttribute($value){
+    return doubleval($value);
+  }
+
+  public function getAccountIdAttribute($value){
+    return intval($value);
+  }
 }
