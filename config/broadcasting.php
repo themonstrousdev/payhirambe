020ecd8859c81880828f5032efcffb058489fce9
kennCK <kennettecanales@gmail.com>
2020-01-10 14:21:41 +0800

    update

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,8 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster'   => 'ap1',
-                // 'encrypted' => true,
+                'encrypted' => true,
+                'useTLS' => true,
                 'host'      => env('PUSHER_HOST'),
                 'port'      => env('PUSHER_PORT'),
                 'scheme'    => env('PUSHER_SCHEME'),
