d599e831bca86c51da97813011d4b3bf90169ee0
kennCK <kennettecanales@gmail.com>
2020-01-21 14:41:49 +0800

    update

--- app/Http/Controllers/RequestValidationController.php
+++ app/Http/Controllers/RequestValidationController.php
@@ -77,7 +77,7 @@ class RequestValidationController extends APIController
       'status'      => $data['status']
     );
     $this->insertDB($insertData);
-    app($this->messengerGroupClass)->broadcastByParams($data['messages']['messenger_group_id'], $data['messages']['account_id'], false);
+    app($this->messengerGroupClass)->broadcastByParams($data['messages']['messenger_group_id'], $data['messages']['account_id'], true);
     return $this->response();
   }
 
